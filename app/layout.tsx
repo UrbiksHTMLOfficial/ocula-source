b105f86455ed31de417d31ab5aaf539a505783e2
UrbiksCODEOfficial <urbiksmusicofficial@gmail.com>
2025-08-16 18:19:39 +0300

    Update layout.tsx
    OCULA

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,8 @@ import { SessionProvider } from 'next-auth/react';
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://chat.vercel.ai'),
-  title: 'Next.js Chatbot Template',
-  description: 'Next.js chatbot template using the AI SDK.',
+  title: 'Ocula',
+  description: 'Ocula is a Next.js AI Hoster.',
 };
 
 export const viewport = {
